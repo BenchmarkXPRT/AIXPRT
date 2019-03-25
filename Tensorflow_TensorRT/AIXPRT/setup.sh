1e6a8bb03294bc07fe594a8a40218b725afb96aa
Tom Barker <tbarker@principledtechnologies.com>
2019-03-25 12:11:00 -0400

    Removed extra cd calls from setup.sh

--- Tensorflow_TensorRT/AIXPRT/setup.sh
+++ Tensorflow_TensorRT/AIXPRT/setup.sh
@@ -1,15 +1,11 @@
-git clone https://github.com/tensorflow/models.git 
-cd models 
-export PYTHONPATH="$PYTHONPATH:$PWD" 
+git clone https://github.com/tensorflow/models.git
+cd models
+export PYTHONPATH="$PYTHONPATH:$PWD"
 cd research
-export PYTHONPATH="$PYTHONPATH:$PWD" 
+export PYTHONPATH="$PYTHONPATH:$PWD"
 wget -O protobuf.zip https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
 unzip protobuf.zip
 ./bin/protoc object_detection/protos/*.proto --python_out=.
-cd models 
-export PYTHONPATH="$PYTHONPATH:$PWD" 
-cd research
-export PYTHONPATH="$PYTHONPATH:$PWD" 
 cd slim
 python setup.py install
 pip install requests pillow
